a22f914fc3f3301ec0d97d62213a440c48569639
geriosla99 <geriosla@gmail.com>
2021-09-03 11:15:27 -0500

    add more things

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Container } from 'react-bootstrap/lib/Tab'
 
 const Login = () => {
     return (
         <>
             <Container>
-                
+
             </Container>
         </>
     )
